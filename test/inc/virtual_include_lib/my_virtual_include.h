c21fbafc9a932ccdac6a3f95eaf7d39b76b35c90
F.Tibor <tibi.furesz@gmail.com>
2025-07-25 13:43:29 +0200

    Revert "Add test for symlink resolution"
    This reverts commit b332b99f9048d2e1e8c68c0397e0d9373b160ef3.

--- test/inc/virtual_include_lib/my_virtual_include.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <iostream>
-
-inline void hello() {
-    std::cout << "Hello from virtual include!" << std::endl;
-    int x = 10/0;
-}
--- test/src/virtual_include.cc
+++
@@ -1,6 +0,0 @@
-#include "virtual_include_lib/my_virtual_include.h"
-
-int main() {
-    hello();
-    return 0;
-}
