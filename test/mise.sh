e74b482285c4667c3728f762a5c7a0484152fcba
Alex <alexkrapivin@gmx.com>
2026-01-11 18:35:53 +0100

    Multiconfiguration test with Mise
    Why:
    We should run all tests for different configurations:
    - Bazel versions 6, 7 and 8
    - CodeChecker versions 6.25, 6.26, 6.27
    - probably different Python versions
    - might be different Clang and Clang-Tidy versions
    
    What:
    - Introduce Mise to support multiple configurations
    - Add example Mise configs for Bazel and CodeChecker
    - Add script which installs, sets up and runs all configs

---
+++ test/mise.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# FIXME: We need MISE_GITHUB_TOKEN to download from GitHub
+#        since URLs are not supported in lock files for pipx
+#        See https://mise.jdx.dev/dev-tools/mise-lock.html#backend-support
+# export MISE_GITHUB_TOKEN="xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+
+if ! command -v mise >/dev/null 2>&1; then
+    echo ">>> Install Mise:"
+    curl https://mise.run | sh
+    eval "$(~/.local/bin/mise activate bash)"
+fi
+mise trust
+mise version
+mise install
+
+echo ">>> Run Test:"
+mise run test:6
+mise run test:7
+mise run test:8
+
