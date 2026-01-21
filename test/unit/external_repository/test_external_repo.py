ad57cf2489cde05563ff770eaeda84fcc2337039
F.Tibor <tibi.furesz@gmail.com>
2026-01-21 13:05:21 +0100

    Rewrite test to work in a temporary directory

--- test/unit/external_repository/test_external_repo.py
+++ test/unit/external_repository/test_external_repo.py
@@ -15,19 +15,20 @@
 """
 Test external repositories with codechecker
 """
-from logging import debug
 import logging
 import os
+import re
 import shutil
+import tempfile
 import unittest
 from typing import final
 from common.base import TestBase
+from pathlib import Path
+
 
 
 class TestImplDepExternalDep(TestBase):
     """Test external repositories with codechecker"""
-    # Set working directory
-    __test_path__ = os.path.dirname(os.path.abspath(__file__))
     BAZEL_BIN_DIR = os.path.join("bazel-bin")
     BAZEL_TESTLOGS_DIR = os.path.join("bazel-testlogs")
     BAZEL_VERSION = None
@@ -39,37 +40,45 @@ class TestImplDepExternalDep(TestBase):
         Copy bazelversion from main, otherwise bazelisk will download the latest
         bazel version.
         """
-        # The folder bazel-external_repository contains this script
-        # and the unittest test discovery finds it.
-        # This is why, it is imperative that these directories get cleared
-        cls.run_command("bazel clean")
-        super().setUpClass()
+        # Set working directory before calling base class!
+        cls.__test_path__ = tempfile.mkdtemp()
         try:
-            shutil.copy("../../../.bazelversion", ".bazelversion")
+            shutil.copytree(
+                os.path.dirname(os.path.abspath(__file__)),
+                cls.__test_path__,
+                dirs_exist_ok=True
+            )
+            module_file = Path(os.path.join(cls.__test_path__, "MODULE.bazel"))
+            content = module_file.read_text().replace(
+                "../../../",
+                f"{os.path.dirname(os.path.abspath(__file__))}/../../../"
+                )
+            module_file.write_text(content)
+        except Exception as e:
+            raise Exception(f"Failed to copy files to the temporary folder {e}")
+        try:
             shutil.copy(
-                "../../../.bazelversion", "third_party/my_lib/.bazelversion")
-
+                os.path.join(
+                    os.path.dirname(os.path.abspath(__file__)),
+                    "../../../",
+                    ".bazelversion",
+                ),
+                os.path.join(cls.__test_path__, ".bazelversion")
+            )
         except:
             logging.debug("No bazel version set, using system default")
-        _, stdout, _ = cls.run_command("bazel --version")
-        cls.BAZEL_VERSION = stdout.split(' ')[2].strip()
-        logging.debug("Using Bazel", cls.BAZEL_VERSION)
+        _, version, _ = cls.run_command("bazel --version")
+        cls.BAZEL_VERSION = re.sub(r".*bazel ", "", version)
+        logging.debug(f"System default is: {cls.BAZEL_VERSION}")
+        super().setUpClass()
 
     @final
     @classmethod
     def tearDownClass(cls):
         """Remove bazelversion from this test"""
         super().tearDownClass()
-        # The folder bazel-external_repository contains this script
-        # and the unittest test discovery finds it.
-        # This is why, it is imperative that these directories get cleared
-        cls.run_command("bazel clean")
         try:
-            os.remove(".bazelversion")
-        except:
-            pass
-        try:
-            os.remove("third_party/my_lib/.bazelversion")
+            os.remove(cls.__test_path__)  #type: ignore
         except:
             pass
 
@@ -83,7 +92,7 @@ class TestImplDepExternalDep(TestBase):
             "compile_commands_isystem",
             "compile_commands.json")
 
-        # The ~override part is a consquence of using Bzlmod.
+        # The ~override part is a consequence of using Bzlmod.
         if self.BAZEL_VERSION.startswith("6"): # type: ignore
             pattern1 = "-isystem external/external_lib~override/include"
             pattern2 = "-isystem " + \
