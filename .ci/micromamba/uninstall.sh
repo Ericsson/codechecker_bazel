6a04046c44794bc039af346b945cc69b9ac6af19
Alex <alexkrapivin@gmx.com>
2026-02-06 22:10:26 +0100

    Add README.md and address issues

--- .ci/micromamba/uninstall.sh
+++ .ci/micromamba/uninstall.sh
@@ -2,7 +2,7 @@ THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 MICROMAMBA=".ci/micromamba"
 if [[ "$(basename $THIS_DIR)" == "$MICROMAMBA" ]]; then
-    PROCESSES="bazel java python3 CodeChecke"
+    PROCESSES="bazel java python3 CodeChecker"
     echo "Killing: $PROCESSES"
     timeout 10s killall --quiet --wait $PROCESSES
     killall --quiet -SIGKILL $PROCESSES
@@ -12,13 +12,6 @@ if [[ "$(basename $THIS_DIR)" == "$MICROMAMBA" ]]; then
     chmod -R +w $THIS_DIR/micromamba
     rm -rf $THIS_DIR/micromamba
 
-    # for directory in $THIS_DIR/*/; do
-    #     if [[ -d "$directory" ]]; then
-    #         echo "Removing: $directory"
-    #         chmod -R +w $directory
-    #         rm -rf $directory
-    #     fi
-    # done
     echo "Please exit current shell session"
 else
     echo "Error: wring location $THIS_DIR"
